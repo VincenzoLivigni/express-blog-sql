11377a1ac7b408abd719dfdebe3b592b5c8fcf58
VincenzoLivigni <enzolivigni13@gmail.com>
2025-12-09 16:04:47 +0100

    add "destroy" to delete posts to MySQL database

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -13,7 +13,7 @@ const index = (req, res) => {
                 dbError: err.message                            // mysql2 genera messaggio d'errore automatico
             })
         }
-        console.log(results)                                    // restituisce i risultati della query nel terminale
+        // console.log(results)                                 // restituisce i risultati della query nel terminale
         res.json(results);                                      // restituisce tutti i post in formato Json (postman)
     })
 }
@@ -94,18 +94,43 @@ const modify = (req, res) => {
 // destroy -> delete 
 const destroy = (req, res) => {
     const id = Number(req.params.id)
-    const foundPost = posts.find((post) => post.id === id)      // cerca il post corrispondente dall'array in base all'id
 
-    if (!foundPost)
-        return res.status(404).json({
-            error: true,
-            message: "psot not found"
-        })
+    const sql = "DELETE FROM posts WHERE id = ?"                        // elimina il post con id specifico
+    console.log(sql, id)
 
-    posts.splice(posts.indexOf(foundPost), 1)                   // rimuove il post che corrisponde all'id dall'array
-    console.log(posts)                                          // stampa nel terminale la lista di post aggiornata (tranne quello eliminato)
-    res.sendStatus(204)                                         // 204 -> risponde con "nessun contenuto"
+    connection.query(sql, [id], (err, results) => {                     // esegue il "delete" sul database prendendo l'id come parametro -- [id] sostituisce il "?"
+        if (err) {                                                      // in caso di errore nella query
+            return res.status(500).json({                               // 500 -> risponde con "errore interno al server"
+                error: true,
+                message: err.message
+            })
+        }
+        console.log(results)                                                    // stampa nel terminale il risultato della query
+
+        if (results.affectedRows === 0) {                                 // se nessuna riga viene eliminata vuol dire che il post non esiste
+            return res.status(404).json({ error: "Post not found" });     // 404 -> risponde con "post non trovato"
+        }
+
+        return res.sendStatus(204);                                       // se l'eliminazione va a buon fine -> 204 "nessun contenuto"
+    })
+
 }
+
+/*
+const foundPost = posts.find((post) => post.id === id)      // cerca il post corrispondente dall'array in base all'id
+
+if (!foundPost)
+    return res.status(404).json({
+        error: true,
+        message: "psot not found"
+    })
+
+posts.splice(posts.indexOf(foundPost), 1)                   // rimuove il post che corrisponde all'id dall'array
+console.log(posts)                                          // stampa nel terminale la lista di post aggiornata (tranne quello eliminato)
+res.sendStatus(204)                                         // 204 -> risponde con "nessun contenuto"
+}
+*/
+
 
 module.exports = {
     index,
